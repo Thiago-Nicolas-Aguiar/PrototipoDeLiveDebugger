7d28e116b09cc1ee3a2da144f6e8b6b6dd642080
Thiago-Nicolas-Aguiar <thiago.nicolas.aguiar@gmail.com>
2026-06-23 19:30:56 -0300

    Agregue animaciones nuevas para el jugador, Salto, Correr, las configure para que dependiendo de la accion cambie correctamente (tambien cambie las teclas por defecto a unas personalizadas [A, D, Space]

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0 
 # Debe ser negativa para que el salto vaya en contra de la gravedad
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,12 +13,29 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Saltar") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("MoverIzquierda", "MoverDerecha")
+	
+	# Cambios de direccion (sprites)
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+
+	#Animaciones
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("Idle")
+		else:
+			animated_sprite.play("Run")
+	else:
+		animated_sprite.play("Jump")
+
+	# Cambios de direccion (jugador)
 	if direction:
 		velocity.x = direction * SPEED
 	else:
